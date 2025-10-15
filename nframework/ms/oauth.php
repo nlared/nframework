538593bd935021680a1a21e7c3158ad7e09ae9bb
nlared <quique@nlared.com>
2025-10-14 22:01:30 -0600

    Feat: Integrate login redirect into Microsoft Generic OAuth flow

--- nframework/ms/oauth.php
+++ nframework/ms/oauth.php
@@ -31,131 +31,143 @@ session_start();
 
 
 
-function normalizar($texto){
-	$texto=str_replace(['á','é','í','ó','ú','ñ','Ñ'],['A','E','I','O','U','N','N'] ,strtoupper(trim($texto)));
-	while(strpos($texto,'  ')!==false){
-		$texto=str_replace('  ',' ',$texto);
+function normalizar($texto)
+{
+	$texto = str_replace(['á', 'é', 'í', 'ó', 'ú', 'ñ', 'Ñ'], ['A', 'E', 'I', 'O', 'U', 'N', 'N'], strtoupper(trim($texto)));
+	while (strpos($texto, '  ') !== false) {
+		$texto = str_replace('  ', ' ', $texto);
 	}
 	return $texto;
 }
 
 
-function normalizar2($texto){
-	$texto=strtoupper(trim($texto));
-	while(strpos($texto,'  ')!==false){
-		$texto=str_replace('  ',' ',$texto);
+function normalizar2($texto)
+{
+	$texto = strtoupper(trim($texto));
+	while (strpos($texto, '  ') !== false) {
+		$texto = str_replace('  ', ' ', $texto);
 	}
 	return $texto;
 }
 
 use Microsoft\Graph\Connect\Constants;
+
 require 'src/Constants.php';
 
 print_r($config);
 
 $provider = new \League\OAuth2\Client\Provider\GenericProvider([
-    'clientId'                => $config['microsoft_oauth_client_id'],
-    'clientSecret'            => $config['microsoft_oauth_client_secret'],
-    'redirectUri'             => 'https://'.$_SERVER['HTTP_HOST'].'/login-microsoft/oauth',
-    'urlAuthorize'            => Constants::AUTHORITY_URL . Constants::AUTHORIZE_ENDPOINT,
-    'urlAccessToken'          => Constants::AUTHORITY_URL . Constants::TOKEN_ENDPOINT,
-    'urlResourceOwnerDetails' => '',
-    'scopes'                  => Constants::SCOPES
+	'clientId'                => $config['microsoft_oauth_client_id'],
+	'clientSecret'            => $config['microsoft_oauth_client_secret'],
+	'redirectUri'             => 'https://' . $_SERVER['HTTP_HOST'] . '/login-microsoft/oauth',
+	'urlAuthorize'            => Constants::AUTHORITY_URL . Constants::AUTHORIZE_ENDPOINT,
+	'urlAccessToken'          => Constants::AUTHORITY_URL . Constants::TOKEN_ENDPOINT,
+	'urlResourceOwnerDetails' => '',
+	'scopes'                  => Constants::SCOPES
 ]);
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && !isset($_GET['code']) && !isset($_GET['error'])) {
-    $authorizationUrl = $provider->getAuthorizationUrl();
+	$authorizationUrl = $provider->getAuthorizationUrl();
 
-    // The OAuth library automaticaly generates a state value that we can
-    // validate later. We just save it for now.
-    $_SESSION['state'] = $provider->getState();
+	// The OAuth library automaticaly generates a state value that we can
+	// validate later. We just save it for now.
+	$_SESSION['state'] = $provider->getState();
 
-    header('Location: ' . $authorizationUrl);
-    exit();
+	header('Location: ' . $authorizationUrl);
+	exit();
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['error'])) {
-    // Answer from the authentication service contains an error.
-    printf('Something went wrong while authenticating: [%s] %s', $_GET['error'], $_GET['error_description']);
+	// Answer from the authentication service contains an error.
+	printf('Something went wrong while authenticating: [%s] %s', $_GET['error'], $_GET['error_description']);
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['code'])) {
-    // Validate the OAuth state parameter
-    if (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['state'])) {
-      //  require 'common.php';
-        session_destroy();
-        //unset($_SESSION['state']);
-        exit('State value does not match the one initially sent <a href="/" class="button">Inicio</a>');    }
+	// Validate the OAuth state parameter
+	if (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['state'])) {
+		//  require 'common.php';
+		session_destroy();
+		//unset($_SESSION['state']);
+		exit('State value does not match the one initially sent <a href="/" class="button">Inicio</a>');
+	}
 
-    // With the authorization code, we can retrieve access tokens and other data.
-    try {
-        // Get an access token using the authorization code grant
-        $accessToken = $provider->getAccessToken('authorization_code', [
-            'code'     => $_GET['code']
-        ]);
-        $_SESSION['access_token'] = $accessToken->getToken(); 
-        // The id token is a JWT token that contains information about the user
-        // It's a base64 coded string that has a header, payload and signature
-        $idToken = $accessToken->getValues()['id_token'];
-        $decodedAccessTokenPayload = base64_decode(
-            explode('.', $idToken)[1]
-        );
-        $jsonAccessTokenPayload = json_decode($decodedAccessTokenPayload, true);
-        // The following user properties are needed in the next page
-        $_SESSION['preferred_username'] = $jsonAccessTokenPayload['preferred_username'];
-        $_SESSION['given_name'] = $jsonAccessTokenPayload['name'];
-        $useroauth=$m->{$config['sitedb']}->users->findOne([
-        	'$or'=>[
-    			['username'=>$jsonAccessTokenPayload['preferred_username']],
-        		['nombrenormalizado'=>normalizar($jsonAccessTokenPayload['name'])]
-        		]
-        	]);
-        if(!empty($useroauth->_id)){
-        	file_put_contents('ola.txt',$decodedAccessTokenPayload);
-        	if($useroauth->username==''){
-        		$m->{$config['sitedb']}->users->updateOne(
-        			['nombrenormalizado'=>normalizar($jsonAccessTokenPayload['name'])],[
-        			'$set'=>[
-        				'username'=>$jsonAccessTokenPayload['preferred_username']]
-        			]);
-    			$_SESSION['user']=$jsonAccessTokenPayload['preferred_username'];
-        	}else{
-        		$m->{$config['sitedb']}->users->updateOne([
-        			'username'=>$jsonAccessTokenPayload['preferred_username']
-        			],[
-        			'$set'=>[
-        				'nombrecompleto'=>normalizar2($jsonAccessTokenPayload['name']),
-        				'nombrenormalizado'=>normalizar($jsonAccessTokenPayload['name']),
-        				'temporal'=>true,
-        				]
-        			]);
-        			
-        		
-        		$_SESSION['user']=(string)$useroauth->_id;
-        	}
-    		session_write_close();
-        	header('Location: /');
-        	
-        }else{
-        	if(strpos($jsonAccessTokenPayload['preferred_username'],'@uadec.edu.mx')!==false){
-        	$newid=new MongoDB\BSON\ObjectID();
-        	$m->{$config['sitedb']}->users->insertOne([
-        		'nombrecompleto'=>normalizar2($jsonAccessTokenPayload['name']),
-        		'nombrenormalizado'=>normalizar($jsonAccessTokenPayload['name']),
-        		'temporal'=>true,
-        		'_id'=>$newid,
-        		'username'=>$jsonAccessTokenPayload['preferred_username']
-        		]);
-        		$_SESSION['user']=(string)$newid;
-	    		header('Location: /');
-			echo '<a href="/">Presiona aqui para continuar</a>';
-        	}else{
-        		session_destroy();
-        		header('Location: /ms/logout.php');
-        		echo 'Esta no es una cuenta oficial<a href="/account/logout.php">Inicio</a>';
-        	}
-        }
-        exit();
-    } catch (League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-        printf('Something went wrong, couldn\'t get tokens: %s', $e->getMessage());
-        	session_destroy();
-        	//require 'common.php';
-        	echo 'Intente de nuevo <a href="/" class="button">Inicio</a>';
-    }
+	// With the authorization code, we can retrieve access tokens and other data.
+	try {
+		// Get an access token using the authorization code grant
+		$accessToken = $provider->getAccessToken('authorization_code', [
+			'code'     => $_GET['code']
+		]);
+		$_SESSION['access_token'] = $accessToken->getToken();
+		// The id token is a JWT token that contains information about the user
+		// It's a base64 coded string that has a header, payload and signature
+		$idToken = $accessToken->getValues()['id_token'];
+		$decodedAccessTokenPayload = base64_decode(
+			explode('.', $idToken)[1]
+		);
+		$jsonAccessTokenPayload = json_decode($decodedAccessTokenPayload, true);
+		// The following user properties are needed in the next page
+		$_SESSION['preferred_username'] = $jsonAccessTokenPayload['preferred_username'];
+		$_SESSION['given_name'] = $jsonAccessTokenPayload['name'];
+		$useroauth = $m->{$config['sitedb']}->users->findOne([
+			'$or' => [
+				['username' => $jsonAccessTokenPayload['preferred_username']],
+				['nombrenormalizado' => normalizar($jsonAccessTokenPayload['name'])]
+			]
+		]);
+		if (!empty($useroauth->_id)) {
+			file_put_contents('ola.txt', $decodedAccessTokenPayload);
+			if ($useroauth->username == '') {
+				$m->{$config['sitedb']}->users->updateOne(
+					['nombrenormalizado' => normalizar($jsonAccessTokenPayload['name'])],
+					[
+						'$set' => [
+							'username' => $jsonAccessTokenPayload['preferred_username']
+						]
+					]
+				);
+				$_SESSION['user'] = $jsonAccessTokenPayload['preferred_username'];
+			} else {
+				$m->{$config['sitedb']}->users->updateOne([
+					'username' => $jsonAccessTokenPayload['preferred_username']
+				], [
+					'$set' => [
+						'nombrecompleto' => normalizar2($jsonAccessTokenPayload['name']),
+						'nombrenormalizado' => normalizar($jsonAccessTokenPayload['name']),
+						'temporal' => true,
+					]
+				]);
+
+
+				$_SESSION['user'] = (string)$useroauth->_id;
+			}
+			session_write_close();
+			if (!empty($_SESSION['login_redirect'])) {
+				$redir = $_SESSION['login_redirect'];
+				unset($_SESSION['login_redirect']);
+				header('Location: ' . $redir);
+				exit;
+			}
+			header('Location: /');
+		} else {
+			if (strpos($jsonAccessTokenPayload['preferred_username'], '@uadec.edu.mx') !== false) {
+				$newid = new MongoDB\BSON\ObjectID();
+				$m->{$config['sitedb']}->users->insertOne([
+					'nombrecompleto' => normalizar2($jsonAccessTokenPayload['name']),
+					'nombrenormalizado' => normalizar($jsonAccessTokenPayload['name']),
+					'temporal' => true,
+					'_id' => $newid,
+					'username' => $jsonAccessTokenPayload['preferred_username']
+				]);
+				$_SESSION['user'] = (string)$newid;
+				header('Location: /');
+				echo '<a href="/">Presiona aqui para continuar</a>';
+			} else {
+				session_destroy();
+				header('Location: /ms/logout.php');
+				echo 'Esta no es una cuenta oficial<a href="/account/logout.php">Inicio</a>';
+			}
+		}
+		exit();
+	} catch (League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+		printf('Something went wrong, couldn\'t get tokens: %s', $e->getMessage());
+		session_destroy();
+		//require 'common.php';
+		echo 'Intente de nuevo <a href="/" class="button">Inicio</a>';
+	}
 }
