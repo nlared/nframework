cd2ae5580e3c880390455bf5f35c2a04f7dc09b0
nlared <quique@nlared.com>
2025-10-17 20:27:48 -0600

    Refactor `inputSpinner` class for readability and consistency

--- includes/class.Base.php
+++ includes/class.Base.php
@@ -407,20 +407,23 @@ class inputNumber extends baseInput
     }
 }
 
-class inputSpinner extends baseInput {
-	public $addclass;
-	public function __toMongo($val){
-		return ($this->data_validate=='integer'||$this->data_validate=='digits'?(int)$val :(float)$val );
-	}
-	
-    public function __toString() {
-    	if($this->validate==''){
-    		$this->validate="number"; //integer,float 
-    	}
-    	if($this->validate=='float'||$this->validate=='number'){
-    		$_SESSION['ANTIXSS'][$this->id]=[FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND];
-    	}else{
-    		/*$_SESSION['ANTIXSS'][$this->id]=[
+class inputSpinner extends baseInput
+{
+    public $addclass;
+    public function __toMongo($val)
+    {
+        return ($this->data_validate == 'integer' || $this->data_validate == 'digits' ? (int)$val : (float)$val);
+    }
+
+    public function __toString()
+    {
+        if ($this->validate == '') {
+            $this->validate = "number"; //integer,float 
+        }
+        if ($this->validate == 'float' || $this->validate == 'number') {
+            $_SESSION['ANTIXSS'][$this->id] = [FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_THOUSAND];
+        } else {
+            /*$_SESSION['ANTIXSS'][$this->id]=[
     			FILTER_VALIDATE_INT,[
 		        'options' => [
 		            'default' => $this->default,
@@ -429,37 +432,39 @@ class inputSpinner extends baseInput {
 		        ],
 		        'flags' => FILTER_FLAG_ALLOW_HEX]
     		];*/
-    		$_SESSION['ANTIXSS'][$this->id]=[
-    			FILTER_VALIDATE_INT
-    		];
-    	}
-    	
-        return ($this->caption!=''?'<label for="'.$this->id.'">'.$this->caption.'</label>':'').
-        '<input type="text" data-role="spinner" id="'.$this->id.
-			'" name="' . $this->name .
-			'"' . $this->writetags() .
-			' data-validate="'.$this->data_validate().'" value="'.$this->value .'"'.
-			($this->datasize ? ' data-size="'.$this->datasize.'"' : '') .
-			($this->required ? ' required="required"' : '') .
-			($this->readonly ? ' readonly="readonly"' : '') .
-			($this->disabled ? ' disabled' : '') . 
-			($this->addclass ? ' class="'.$this->addclass.'"' : '') .
-			' autocomplete="off">';
+            $_SESSION['ANTIXSS'][$this->id] = [
+                FILTER_VALIDATE_INT
+            ];
+        }
+
+        return ($this->caption != '' ? '<label for="' . $this->id . '">' . $this->caption . '</label>' : '') .
+            '<input type="text" data-role="spinner" id="' . $this->id .
+            '" name="' . $this->name .
+            '"' . $this->writetags() .
+            ' data-validate="' . $this->data_validate() . '" value="' . $this->value . '"' .
+            ($this->datasize ? ' data-size="' . $this->datasize . '"' : '') .
+            ($this->required ? ' required="required"' : '') .
+            ($this->readonly ? ' readonly="readonly"' : '') .
+            ($this->disabled ? ' disabled' : '') .
+            ($this->addclass ? ' class="' . $this->addclass . '"' : '') .
+            ' autocomplete="off">';
     }
-	public function is_valid($newval) {
-		return is_numeric($newval);	
-	}
-    
+    public function is_valid($newval)
+    {
+        return is_numeric($newval);
+    }
 }
 
-class inputRating extends baseInput {
-    public $tags=['data-values','onchange'];
+class inputRating extends baseInput
+{
+    public $tags = ['data-values', 'onchange'];
     public $onchange;
-    public function __toString() {
-        return ($this->caption!=''?'<label for="'.$this->id.'">'.$this->caption.'</label>':'').
-        '<input data-role="rating"  id="'.$this->id.
+    public function __toString()
+    {
+        return ($this->caption != '' ? '<label for="' . $this->id . '">' . $this->caption . '</label>' : '') .
+            '<input data-role="rating"  id="' . $this->id .
             '" name="' . $this->name .
-            '"' . $this->writetags() .' data-value="'.$this->value.'">';
+            '"' . $this->writetags() . ' data-value="' . $this->value . '">';
     }
 }
 
