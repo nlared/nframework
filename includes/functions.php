92b29424e8a28414f11354273273e017f3a8601e
nlared <quique@nlared.com>
2025-10-26 01:33:56 -0600

    Feat: Add file system utility functions

--- includes/functions.php
+++ includes/functions.php
@@ -429,3 +429,55 @@ function isValidSession($encryptedSessionId, $key)
 
     return session_status() === PHP_SESSION_ACTIVE;
 }
+
+function delTree($dir)
+{
+    $files = array_diff(scandir($dir), array('.', '..'));
+    foreach ($files as $file) {
+        (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+    }
+    return rmdir($dir);
+}
+
+
+function GetDirectorySize($path)
+{
+    $bytestotal = 0;
+    $path = realpath($path);
+    if ($path !== false && $path != '' && file_exists($path)) {
+        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object) {
+            $bytestotal += $object->getSize();
+        }
+    }
+    return $bytestotal;
+}
+
+function formatSize($bytes, $decimals = 2)
+{
+    $size = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+    $factor = floor((strlen($bytes) - 1) / 3);
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+}
+
+function nfurlencode($rb)
+{
+    $rb = urlencode($rb);
+    ## Sustituyo caracteres en la cadena final
+    $rb = str_replace("Ã¡", "&aacute;", $rb);
+    $rb = str_replace("Ã©", "&eacute;", $rb);
+    $rb = str_replace("Â®", "&reg;", $rb);
+    $rb = str_replace("Ã­", "&iacute;", $rb);
+    $rb = str_replace("ï¿½", "&iacute;", $rb);
+    $rb = str_replace("Ã³", "&oacute;", $rb);
+    $rb = str_replace("Ãº", "&uacute;", $rb);
+    $rb = str_replace("n~", "&ntilde;", $rb);
+    $rb = str_replace("Âº", "&ordm;", $rb);
+    $rb = str_replace("Âª", "&ordf;", $rb);
+    $rb = str_replace("ÃƒÂ¡", "&aacute;", $rb);
+    $rb = str_replace("Ã±", "&ntilde;", $rb);
+    $rb = str_replace("Ã‘", "&Ntilde;", $rb);
+    $rb = str_replace("ÃƒÂ±", "&ntilde;", $rb);
+    $rb = str_replace("n~", "&ntilde;", $rb);
+    $rb = str_replace("Ãš", "&Uacute;", $rb);
+    return $rb;
+}
