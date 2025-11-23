ba25789bf573fbf026bcb6d977afb4e85c866209
nlared <quique@nlared.com>
2025-11-23 00:29:33 -0600

    Chore: Add blank line in inputFiles JS block for readability

--- includes/class.Base.php
+++ includes/class.Base.php
@@ -1319,6 +1319,7 @@ class inputFiles extends BaseFileInput
                 var html = '';
                 {$this->onDone}
             }
+            
         JS, 'ready');
 
         $this->addBaseJavaScript();
