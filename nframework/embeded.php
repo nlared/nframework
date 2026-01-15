d8dc3ad7e3f6bed52b93d1c9a36fe0e762b53f08
nlared <quique@nlared.com>
2026-01-15 00:44:43 -0600

    feat: Initialize result status to 'init'

--- nframework/embeded.php
+++ nframework/embeded.php
@@ -24,7 +24,7 @@ function getParentPositions(array $nodes, string $startId): array
 use Twig\Environment;
 use Twig\Extension\StringLoaderExtension;
 
-$result = ['status' => 'ok', 'items' => []];
+$result = ['status' => 'init', 'items' => []];
 function get_data($dataset, string $field)
 {
     $parts = explode('.', $field);
