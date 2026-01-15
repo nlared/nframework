355fc8d5cbc0d922f9f0819f35204491643513f4
nlared <quique@nlared.com>
2026-01-15 00:22:14 -0600

    Refactor: Rename `item` key to `items` in initial `$result` array

--- nframework/embeded.php
+++ nframework/embeded.php
@@ -23,7 +23,7 @@ function getParentPositions(array $nodes, string $startId): array
 use Twig\Environment;
 use Twig\Extension\StringLoaderExtension;
 
-$result = ['status' => 'ok', 'item' => []];
+$result = ['status' => 'ok', 'items' => []];
 function get_data($dataset, string $field)
 {
     $parts = explode('.', $field);
