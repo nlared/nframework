c7a5fd8cfb8e7768964e0341bd4395f4f6978cd5
nlared <quique@nlared.com>
2026-01-16 00:18:45 -0600

    feat: Add `downloadfrom` utility function for secure file downloads

--- includes/include.php
+++ includes/include.php
@@ -365,6 +365,25 @@ class class_nframework
             exit();
         }
     }
+    public function downloadfrom($filename): void
+    {
+        header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+        $filename = clean_filename($filename);
+        if (file_exists($filename)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: ' . mime_content_type($filename));
+            header('Content-Disposition: attachment; filename="' . basename($filename) . '"');            
+            header('Content-Length: ' . filesize($filename));
+            readfile($filename);
+            exit;
+        } else {
+            throw new nFrameworkException("File not found: " . $filename);
+        }
+    }
 
     public function language()
     {
