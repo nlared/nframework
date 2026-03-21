21d5bf0d3e336128e0b8f7b3455462efcda090cf
nlared <quique@nlared.com>
2026-03-20 21:26:07 -0600

    docs: Add example script for Word template to PDF conversion

---
+++ docs/docttopdf.php
@@ -0,0 +1,6 @@
+<?
+require 'include.php';
+$wordtemplate = new \PhpOffice\PhpWord\TemplateProcessor('template.docx');
+$wordtemplate->setValue('name', 'John Doe');
+$wordtemplate->setValue('datetime',  date('Y-m-d H:i:s'));
+$nframework->wordTemplateOutPdf($wordtemplate, 'output.pdf');
