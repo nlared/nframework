b2e72f84b1bf3996c13ae89737d5dc4ebadeeb21
nlared <quique@nlared.com>
2026-04-16 19:06:41 -0600

    feat: Implement NFDialg for single record management

---
+++ nframework/nfdialog.php
@@ -0,0 +1,70 @@
+<?
+
+$tabla = $m->{$config['sitedb']}->ntablas->findOne([
+    '_id' => tomongoid($p['collection'])
+]);
+
+if (!$tabla) {
+    echo 'No se encontró la tabla';
+}
+if (!isset($tabla->campos) || count($tabla->campos) == 0) {
+    echo 'La tabla no tiene campos definidos';
+}
+
+
+$dataset = new dataset(
+    [
+        'collection' => $m->{$config['sitedb']}->{$tabla->nfcollection},
+        '_id' => $_GET['_id'],
+        'simpleid' => false,
+        'nameprefix' => 'data'
+    ]
+);
+
+foreach ($tabla->campos as $campo) {
+    $className = $campo->tipo;
+    $elements[] = new $className([
+        'field' => $campo->nombre,
+        'caption' => $campo->descripcion_corta,
+        'required' => $campo->required
+    ]);
+}
+
+if ($nframework->isAjax()) {
+    if ($_POST['op'] == 'save') {
+        try {
+            $result = [
+                'error' => $dataset->save(),
+            ];
+        } catch (Exception $e) {
+            $result = [
+                'error' => $e->getMessage()
+            ];
+        }
+    }
+} else {
+    $nframework->usecommon = true;
+
+
+?>
+    <div class="container">
+        <?= secureform() ?>
+        <div class="box shadow-large">
+            <div class="box-title"><?= $tabla->singular  ?></div>
+            <div class="grid">
+                <?
+                foreach ($elements as $element) {
+                    echo '<div class="cell">' . $element . '</div>';
+                }
+                ?>
+                <div class="row">
+                    <div class="cell-md-2 offset-md-8"><a href="/nftable/" class="button primary btn btn-primary w-100"><span class="mif-exit"></span>&nbsp;<?= $lng['close']  ?>?></a></div>
+                    <div class="cell-md-2"><button class="button success btn btn-success secureop  w-100" value="save"><span class="mif-floppy-disk"></span>&nbsp;<?= $lng['save']  ?>?></button></div>
+                </div>
+            </div>
+        </div>
+        </form>
+    </div>
+<?
+}
+?>
