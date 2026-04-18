111535711fd29553cc3abbb84c2d1ff8f441de15
nlared <quique@nlared.com>
2026-04-17 20:24:33 -0600

    feat: Add endpoint for NFTables column preview

---
+++ admin/nftables/preview.php
@@ -0,0 +1,16 @@
+<?php
+require 'include.php';
+$tabla = $m->{$config['sitedb']}->nftables->findOne([
+    '_id' => tomongoid($_GET['_id'])
+]);
+if (!$tabla) {
+    echo 'No se encontró la tabla';
+}
+if (!isset($tabla->fields) || count($tabla->fields) == 0) {
+    echo 'La tabla no tiene campos definidos';
+}
+foreach ($tabla->fields as $field) {
+    $columns[] = $field->field;
+}
+$result['columns'] = $columns;
+echo json_encode($result);
