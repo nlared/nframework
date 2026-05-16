296604f9b51d30376a5a4afc54e1851b9d0fd9f0
nlared <quique@nlared.com>
2026-05-15 21:58:40 -0600

    feat: Create server-side endpoint for Select AJAX options

---
+++ nframework/select_ajax.php
@@ -0,0 +1,31 @@
+<?php
+//$developermode=true;
+require_once 'include.php';
+$datainfo = $_SESSION['selectajax'][$_GET['id']];
+if (empty($datainfo)) {
+    echo 'error en session';
+    die();
+}
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+$options = [];
+$result = [];
+foreach ($datainfo['columns'] as $cno => $co) {
+    $matchs[][$co] = MongoDB\BSON\Regex($_GET['q'], "i");
+}
+$pipeline[] = ['$match' => ['$or' => $matchs]];
+$pipeline = array_merge($datainfo['pipeline'], $pipeline);
+$pipeline[] = ['$addFields' => ['label' => $datainfo['label'], 'value' => $datainfo['value']]];
+$pipeline[] = ['$project' => ['_id' => 0, 'label' => 1, 'value' => 1]];
+
+
+try {
+    $result = $m->{$datainfo['db']}->{$datainfo['collection']}->aggregate($pipeline, $options)->toArray();
+} catch (Exception $e) {
+    $error = 'Error en la consulta: ' . $e->getMessage();
+}
+if ($error) {
+    $result['error'] = $error;
+}
+echo json_encode($result);
