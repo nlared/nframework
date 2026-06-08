1056b8d1cf97ff2edede548c3b2b6fba63cb8a98
nlared <quique@nlared.com>
2026-06-08 10:47:00 -0600

    Fix: Correct 'mixed' type hint syntax in datatable pipeline

--- nframework/datatablef.php
+++ nframework/datatablef.php
@@ -35,7 +35,7 @@ if (!empty($_POST['pipelinequery'])) {
 	foreach ($filters as $filter) {
 		if (isset($filter['field'])) {
 			if ($filter['phptype'] == 'number') {
-				$tmppipeline = converttophptypepipeline($tmppipeline, $filter['field'], 'function($mixed $value): float {return floatval($value);};');
+				$tmppipeline = converttophptypepipeline($tmppipeline, $filter['field'], 'function(mixed $value): float {return floatval($value);};');
 			}
 		}
 	}
