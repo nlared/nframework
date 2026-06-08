f0715f306decee4a5a6411825aba113f43abdc47
nlared <quique@nlared.com>
2026-06-08 12:27:51 -0600

    Style: Apply minor formatting and whitespace fixes

--- includes/include.php
+++ includes/include.php
@@ -513,13 +513,13 @@ if ($block_reason != "") {
     http_response_code(403);
     exit("Access denied.");
 }
-$nfuristat=$m->{$config['sitedb']}->nfuristats->insertOne([
+$nfuristat = $m->{$config['sitedb']}->nfuristats->insertOne([
     'created_at' => new MongoDB\BSON\UTCDateTime(time() * 1000), // use PHP DateTime; the MongoDB driver will convert it to BSON UTC datetime
     'ip' => $ip,
     'host' => $_SERVER['HTTP_HOST'],
     'path' => $_SERVER['REQUEST_URI'],
     'agent' => $_SERVER['HTTP_USER_AGENT'] ?? '',
-    'block_reason' => $block_reason 
+    'block_reason' => $block_reason
 ]);
 $rules = [['host' => ['$exists' => false]]];
 foreach (
