50fa9c60b2f4fbdf12d003c1379a293d1000e8d9
nlared <quique@nlared.com>
2026-06-17 19:36:59 -0600

    Test: Add and update XMLS unit tests

--- tests/Unit/XMLSGeneratorTest.php
+++ tests/Unit/XMLSGeneratorTest.php
@@ -8,15 +8,44 @@ require_once __DIR__ . '/../../includes/class.XMLS.php';
 
 class XMLSGeneratorTest extends TestCase
 {
-    public function testValidateWithXsdReturnsTrueForValidXml(): void
-    {
-        $tmpDir = sys_get_temp_dir() . '/nframework_xmls_' . uniqid('', true);
-        mkdir($tmpDir, 0775, true);
+  public function testToXmlStringThrowsWhenAttributeIsArray(): void
+  {
+    $xmlObject = new class() extends \XMLS {
+      public $tagName = 'item';
+      public $attributes = ['id'];
+      public $id = ['invalid'];
+    };
 
-        $xsdPath = $tmpDir . '/schema.xsd';
-        file_put_contents(
-            $xsdPath,
-            <<<XSD
+    $this->expectException(\RuntimeException::class);
+    $this->expectExceptionMessage('Attribute id cannot be an array');
+
+    $xmlObject->toXmlString();
+  }
+
+  public function testToStringReturnsVisibleErrorAndStoresLastSerializationError(): void
+  {
+    $xmlObject = new class() extends \XMLS {
+      public $tagName = 'item';
+      public $attributes = ['id'];
+      public $id = ['invalid'];
+    };
+
+    $xml = (string)$xmlObject;
+
+    $this->assertStringContainsString('XMLS serialization error', $xml);
+    $this->assertStringContainsString('Attribute id cannot be an array', $xml);
+    $this->assertSame('Attribute id cannot be an array', $xmlObject->lastSerializationError);
+  }
+
+  public function testValidateWithXsdReturnsTrueForValidXml(): void
+  {
+    $tmpDir = sys_get_temp_dir() . '/nframework_xmls_' . uniqid('', true);
+    mkdir($tmpDir, 0775, true);
+
+    $xsdPath = $tmpDir . '/schema.xsd';
+    file_put_contents(
+      $xsdPath,
+      <<<XSD
 <?xml version="1.0" encoding="UTF-8"?>
 <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
   <xs:element name="item">
@@ -29,32 +58,32 @@ class XMLSGeneratorTest extends TestCase
   </xs:element>
 </xs:schema>
 XSD
-        );
+    );
 
-        $xmlObject = new class() extends \XMLS {
-            public $tagName = 'item';
-            public $attributes = ['id'];
-            public $_sequence = ['name'];
-            public $id = 'A1';
-            public $name = '<name>Test</name>';
-        };
+    $xmlObject = new class() extends \XMLS {
+      public $tagName = 'item';
+      public $attributes = ['id'];
+      public $_sequence = ['name'];
+      public $id = 'A1';
+      public $name = '<name>Test</name>';
+    };
 
-        $errors = [];
-        $isValid = $xmlObject->validateWithXsd($xsdPath, $errors);
+    $errors = [];
+    $isValid = $xmlObject->validateWithXsd($xsdPath, $errors);
 
-        $this->assertTrue($isValid);
-        $this->assertSame([], $errors);
-    }
+    $this->assertTrue($isValid);
+    $this->assertSame([], $errors);
+  }
 
-    public function testValidateWithXsdReturnsFalseForInvalidXml(): void
-    {
-        $tmpDir = sys_get_temp_dir() . '/nframework_xmls_' . uniqid('', true);
-        mkdir($tmpDir, 0775, true);
+  public function testValidateWithXsdReturnsFalseForInvalidXml(): void
+  {
+    $tmpDir = sys_get_temp_dir() . '/nframework_xmls_' . uniqid('', true);
+    mkdir($tmpDir, 0775, true);
 
-        $xsdPath = $tmpDir . '/schema.xsd';
-        file_put_contents(
-            $xsdPath,
-            <<<XSD
+    $xsdPath = $tmpDir . '/schema.xsd';
+    file_put_contents(
+      $xsdPath,
+      <<<XSD
 <?xml version="1.0" encoding="UTF-8"?>
 <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
   <xs:element name="item">
@@ -67,32 +96,32 @@ XSD
   </xs:element>
 </xs:schema>
 XSD
-        );
+    );
 
-        $xmlObject = new class() extends \XMLS {
-            public $tagName = 'item';
-            public $attributes = ['id'];
-            public $_sequence = ['name'];
-            public $id = '';
-            public $name = '';
-        };
+    $xmlObject = new class() extends \XMLS {
+      public $tagName = 'item';
+      public $attributes = ['id'];
+      public $_sequence = ['name'];
+      public $id = '';
+      public $name = '';
+    };
 
-        $errors = [];
-        $isValid = $xmlObject->validateWithXsd($xsdPath, $errors);
+    $errors = [];
+    $isValid = $xmlObject->validateWithXsd($xsdPath, $errors);
 
-        $this->assertFalse($isValid);
-        $this->assertNotEmpty($errors);
-    }
+    $this->assertFalse($isValid);
+    $this->assertNotEmpty($errors);
+  }
 
-    public function testGenerateClassesFromXsdCreatesClassFiles(): void
-    {
-        $tmpDir = sys_get_temp_dir() . '/nframework_xmls_' . uniqid('', true);
-        mkdir($tmpDir, 0775, true);
+  public function testGenerateClassesFromXsdCreatesClassFiles(): void
+  {
+    $tmpDir = sys_get_temp_dir() . '/nframework_xmls_' . uniqid('', true);
+    mkdir($tmpDir, 0775, true);
 
-        $xsdPath = $tmpDir . '/schema.xsd';
-        file_put_contents(
-            $xsdPath,
-            <<<XSD
+    $xsdPath = $tmpDir . '/schema.xsd';
+    file_put_contents(
+      $xsdPath,
+      <<<XSD
 <?xml version="1.0" encoding="UTF-8"?>
 <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
   <xs:element name="product">
@@ -106,29 +135,29 @@ XSD
   </xs:element>
 </xs:schema>
 XSD
-        );
+    );
 
-        $outputDir = $tmpDir . '/generated';
-        $generated = \XMLS::generateClassesFromXsd(
-            $xsdPath,
-            $outputDir,
-            'Tmp\\Generated',
-            ['overwrite' => true]
-        );
+    $outputDir = $tmpDir . '/generated';
+    $generated = \XMLS::generateClassesFromXsd(
+      $xsdPath,
+      $outputDir,
+      'Tmp\\Generated',
+      ['overwrite' => true]
+    );
 
-        $this->assertNotEmpty($generated);
+    $this->assertNotEmpty($generated);
 
-        $generatedFile = $outputDir . '/Product.php';
-        $this->assertFileExists($generatedFile);
+    $generatedFile = $outputDir . '/Product.php';
+    $this->assertFileExists($generatedFile);
 
-        require_once $generatedFile;
+    require_once $generatedFile;
 
-        $this->assertTrue(class_exists('Tmp\\Generated\\Product'));
-        $object = new \Tmp\Generated\Product();
+    $this->assertTrue(class_exists('Tmp\\Generated\\Product'));
+    $object = new \Tmp\Generated\Product();
 
-        $this->assertSame('product', $object->tagName);
-        $this->assertSame(['id'], $object->attributes);
-        $this->assertSame(['title', 'tag'], $object->_sequence);
-        $this->assertSame([], $object->tag);
-    }
+    $this->assertSame('product', $object->tagName);
+    $this->assertSame(['id'], $object->attributes);
+    $this->assertSame(['title', 'tag'], $object->_sequence);
+    $this->assertSame([], $object->tag);
+  }
 }
