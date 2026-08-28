5c3322e0a51d5ae62ddead36130ca2ed832a9f8b
nlared <quique@nlared.com>
2026-08-28 09:06:04 -0600

    Temporarily set certificate revocation status to false

--- sat/upload.php
+++ sat/upload.php
@@ -81,6 +81,7 @@ if ( 0 < $_FILES['file']['error'] ) {
 	}
 	$revocado=('good'!=trim(substr($res[0],$pos))?false:true);
 	*/
+	$revocado=false;
 	if(!file_exists(__DIR__.'/certs/' ))
 		mkdir(__DIR__.'/certs/');		
 	move_uploaded_file($_FILES['file']['tmp_name'], __DIR__.'/certs/' . $no_cert);
